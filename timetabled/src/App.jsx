3f2891a6b35a06232590e449e86a05e52ba078b8
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-02-29 10:33:06 -0500

    import component into app

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Calendar from './components/Calendar'
 
 const App = () => {
 
@@ -6,7 +7,7 @@ const App = () => {
     <div className="App">
       <h1>My Weekly Plan</h1>
       <h2>From 2024.2.25 to 2024.3.2</h2>
-
+      <Calendar />
     </div>
   )
 }
