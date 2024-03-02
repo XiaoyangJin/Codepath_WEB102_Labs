cee56051f1051cc5ad926447d79ed7e6d51bfd37
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-02 14:45:45 -0500

    Add a counter

--- samosa_selector/src/App.jsx
+++ samosa_selector/src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css';
+import { useState } from 'react';
+
+
 
 const App = () => {
+
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+
+  const updateCount = () => setCount(count + multiplier);
 
   return (
     <div className="App">
       <div className='header'>
         <h1>Samosa Selector</h1>
-        <h2>Count:</h2>
-        <img className='samosa' src='src/image/samosa.jpg'></img>
+        <h2>Count: {count}</h2>
+        <img className='samosa' src='src/image/samosa.jpg' onClick={updateCount} />
       </div>
     </div>
   )
