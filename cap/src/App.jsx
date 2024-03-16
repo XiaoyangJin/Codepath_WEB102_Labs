7eaf81d8813a5d1e7ae6f1289d0c504c86b1dc8c
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-16 14:50:14 -0400

    Get info through api

--- cap/src/App.jsx
+++ cap/src/App.jsx
@@ -1,15 +1,43 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
+import APIForm from './components/APIForm';
+
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+const submitForm = () => {
+
+
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
 
   return (
-    <>
+    <div className="whole-page">
+      <h1>Build Your Own Screenshot! 📸</h1>
 
-    </>
-  )
+      <APIForm
+        inputs={inputs}
+        handleChange={(e) =>
+          setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value.trim(),
+          }))
+        }
+        onSubmit={submitForm}
+      />
+      <br></br>
+
+    </div>
+  );
 }
 
 export default App
