08d5ee354586f8c07f77d7a2c3eb212ce61501b9
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-23 12:28:35 -0400

    clean up input after submit(not refresh)

--- course_practice/src/App.jsx
+++ course_practice/src/App.jsx
@@ -15,8 +15,10 @@ function App() {
   }
 
   const addRecord = (e) => {
+    // since there is no place to store data, we need to prevent refresh to lose data
     e.preventDefault();
     setRecord([...record, form]);
+    setForm({ name: '', cuisine: '' });
   }
 
   return (
@@ -44,9 +46,9 @@ function App() {
       <h2>My Restaurant</h2>
       {
         record.length === 0 ? <h3>Nothing here yet</h3> :
-          record.map((record) => {
+          record.map((record, i) => {
             return (
-              <div style={{ border: '2px solid red', padding: '1rem' }}>
+              <div key={i} style={{ border: '2px solid red', padding: '1rem' }}>
                 <h4>{record.name}</h4>
                 <h4>{record.cuisine}</h4>
               </div>
