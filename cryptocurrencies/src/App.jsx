e148eef59b05c440177f930637ad9fc05d0de414
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-23 14:47:25 -0400

    Add search function and style

--- cryptocurrencies/src/App.jsx
+++ cryptocurrencies/src/App.jsx
@@ -9,6 +9,24 @@ const API_KEY = import.meta.env.VITE_APP_API_KEY;
 function App() {
   const [list, setList] = useState();
 
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  const searchItems = searchValue => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredData = Object.keys(list.Data).filter((item) =>
+        Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      )
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(Object.keys(list.Data));
+    }
+  };
+
   useEffect(() => {
     const fetchAllCoinData = async () => {
       const response = await fetch(
@@ -25,16 +43,31 @@ function App() {
     <>
       <div className="whole-page">
         <h1>My Crypto List</h1>
-        <ul>
-          {list && Object.entries(list.Data).map(([coin]) =>
-            list.Data[coin].PlatformType === "blockchain" ? (
-              <CoinInfo
-                image={list.Data[coin].ImageUrl}
-                name={list.Data[coin].FullName}
-                symbol={list.Data[coin].Symbol}
-              />
-            ) : null
-          )}
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(inputString) => searchItems(inputString.target.value)}
+        />
+        <ul className='coin__list'>
+          {searchInput.length > 0
+            ? filteredResults.map((coin) =>
+              list.Data[coin].PlatformType === "blockchain" ?
+                <CoinInfo
+                  image={list.Data[coin].ImageUrl}
+                  name={list.Data[coin].FullName}
+                  symbol={list.Data[coin].Symbol}
+                />
+                : null
+            )
+            : list && Object.entries(list.Data).map(([coin]) =>
+              list.Data[coin].PlatformType === "blockchain" ?
+                <CoinInfo
+                  image={list.Data[coin].ImageUrl}
+                  name={list.Data[coin].FullName}
+                  symbol={list.Data[coin].Symbol}
+                />
+                : null
+            )}
         </ul>
       </div>
     </>
