6b3129b6fd426a3d512cd8b77d7dd160a7ed6b5b
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-23 14:24:05 -0400

    Create CoinInfo.jsx

---
+++ cryptocurrencies/src/Components/CoinInfo.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+
+const CoinInfo = ({ image, name, symbol }) => {
+    const [price, setPrice] = useState();
+}
+
+export default CoinInfo;
