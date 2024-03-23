1532e98e13d2b775acbd993c8fe6670dfc1f9655
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-23 14:51:11 -0400

    Add some space between name and cost

--- cryptocurrencies/src/Components/CoinInfo.jsx
+++ cryptocurrencies/src/Components/CoinInfo.jsx
@@ -27,7 +27,7 @@ const CoinInfo = ({ image, name, symbol }) => {
                         src={`https://www.cryptocompare.com${image}`}
                         alt={`Small icon for ${name} crypto coin`}
                     />
-                    {name} <span className="tab"></span> ${price.USD} USD
+                    {name} &lt;<span className="tab"></span> ${price.USD} USD
                 </li>
             ) :
                 null
