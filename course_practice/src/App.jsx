05b4aeacd70d6d94f43540de7bcdcad450c24ba9
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-23 11:33:58 -0400

    Try like in count but not passing in object

--- course_practice/src/App.jsx
+++ course_practice/src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState({ likes: 1 })
 
   function handleCount() {
-    setCount(count + 1);
+    count.likes++;
+    setCount(count);
   }
 
   return (
     <>
-      <h1>Count: {count}</h1>
+      <h1>Count: {count.likes}</h1>
       <button onClick={handleCount}>Set count</button>
     </>
   )
