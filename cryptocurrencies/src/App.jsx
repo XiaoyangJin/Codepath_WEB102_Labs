ba892a3d25f046f488db7bb0fd2b8ad39bb0105b
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-23 14:15:29 -0400

    Query the CryptoCompare API to get a list of all the coins

--- cryptocurrencies/src/App.jsx
+++ cryptocurrencies/src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react';
 
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState();
+
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch(
+        "https://min-api.cryptocompare.com/data/all/coinlist?&api_key"
+        + API_KEY
+      );
+      const json = await response.json();
+      setList(json);
+    }
+    fetchAllCoinData().catch(console.error);
+  }, []);
 
   return (
     <>
